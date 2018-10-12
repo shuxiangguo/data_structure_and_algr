6403a1e2adca6d8c442d07c2d676397b63387ae0
shuxiangguo <shuxiangguo7@gmail.com>
2018-10-12 09:38:06 +0800

    add dijkstra.py

--- 图解算法/ch07/Dijkstra.py
+++ 图解算法/ch07/Dijkstra.py
@@ -1,1 +1,59 @@
 # Dijkstra找到的是总权重最小的路径
+# 使用条件：有向无还图DAG(direct acyclic graph)
+
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parent = {}
+parent["a"] = "start"
+parent["b"] = "start"
+parent["fin"] = None
+
+processed = []
+
+
+def find_lowest_cost_node(costs):
+	lowest_cost = float("inf")
+	lowest_cost_node = None
+	for node in costs:
+		cost = costs[node]
+		if cost < lowest_cost and node not in processed:
+			lowest_cost = cost
+			lowest_cost_node = node
+	return lowest_cost_node
+
+
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+	cost = costs[node]
+	neighbors = graph[node]
+
+	# 遍历当前节点的所有邻居
+	for n in neighbors.keys():
+		new_cost = cost + neighbors[n]
+		if costs[n] > new_cost:
+			costs[n] = new_cost
+			parent[n] = node
+	processed.append(node)
+	node = find_lowest_cost_node(costs)
+
+print(cost['a'])
+
+
